a8998b964a4e0bb37c7ef89af050faaaf7e003ae
Morgan Langenhagen <mel73@duke.edu>
2020-10-16 15:49:32 -0400

    updating app.py and templates

--- app/app.py
+++ app/app.py
@@ -2,14 +2,34 @@ import sqlalchemy
 from flask import Flask, render_template, redirect, url_for
 # import models
 import forms
+from models import Users
 
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:21bH1267@vcm-17138.vm.duke.edu/TutorProject'
 
+# @app.route('/')
+# def hello_world():
+#   return 'Hello, World!'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
+
+
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/tutors')
+def tutors():
+    return render_template(
+        'all_tutors.html',
+        users=Users.query.all()
+    )
+
+
 # def all_tutors():
 #   tutors = db.session.query(models.Tutors).all()
 #   return render_template('all-tutors.html', tutors=tutors)                        #implement all-tutors.html template
@@ -36,5 +56,8 @@ def hello_world():
 
 # # mysql://username:password@server/db
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(debug=True)
