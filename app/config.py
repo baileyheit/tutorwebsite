f3a46d90d2f4299cca002776945196e7204322be
Bailey Heit <bailey.heit@duke.edu>
2020-11-01 15:26:37 -0500

    finish 3

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-
-    
-SQLALCHEMY_DATABASE_URI = 'mysql://root:21bH1267@vcm-17138.vm.duke.edu/TutorProject'
-SQLALCHEMY_ECHO = True
-DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:21bH1267@vcm-17138.vm.duke.edu/TutorProject'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    DEBUG = True
 
