2111554b444b2e7ae86be4474a9fad63d6db1add
Bailey Heit <bailey.heit@duke.edu>
2020-11-01 15:08:53 -0500

    the right way

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config.from_object(Config)
 
-import routes
+from app import routes
