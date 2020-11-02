89e5907e8d6b3d703bdbc522b5f9bf5485d3ab12
Bailey Heit <bailey.heit@duke.edu>
2020-11-01 22:10:43 -0500

    fuck you

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import routes, models
-# from app import WebService
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect,  url_for
 from app import app
-from forms import LoginForm
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
