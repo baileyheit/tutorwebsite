29849d0047d2581146dbd79f46effbcba5e46044
Bailey Heit <bailey.heit@duke.edu>
2020-11-01 23:49:49 -0500

    bruh

--- app/models.py
+++ app/models.py
@@ -1,22 +1,28 @@
 
 from sqlalchemy import Column, Integer, String, sql, orm, VARCHAR, Float, BIGINT
-from database import Base
-
+from app.database import Base
 
 class User(Base):
     __tablename__ = 'User'
-    user_name = Column('username', String(50), primary_key=True)
-    password = Column('password', String(50))
+    # id = Column(db.Integer, primary_key=True)
+    # username = Column(db.String(64), index=True, unique=True)
+    # email = Column(db.String(120), index=True, unique=True)
+    # password_hash = db.Column(db.String(128))
+
+    id = Column('id', BIGINT, primary_key=True)
+    username = Column('username', String(50), primary_key=True)
     email = Column('email', String(50))
-    first_name = Column('first_name', String(50))
-    last_name = Column('last_name', String(50))
+    password_hash = Column('password_hash', String(128))
+
+    # first_name = Column('first_name', String(50))
+    # last_name = Column('last_name', String(50))
 
     def __init__(self, username=None, password=None, email=None, first_name=None, last_name=None):
         self.username = username
         self.password = password
         self.email = email
-        self.first_name = first_name
-        self.last_name = last_name
+        # self.first_name = first_name
+        # self.last_name = last_name
 
     def __repr__(self):
         return '<User %r>' % (self.model)
