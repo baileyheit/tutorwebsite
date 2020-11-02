8423bdc9652f16d7c239898af30aa4189dc93454
Bailey Heit <bailey.heit@duke.edu>
2020-11-01 23:27:22 -0500

    fooL

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,12 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from database import db 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 app.config.from_object(Config)
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import routes, models
---
+++ app/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# mysql://root:21bH1267@vcm-17138.vm.duke.edu/TutorProject
+engine = create_engine(
+    'mysql+pymysql://newuser:pass@vcm-17138.vm.duke.edu/TutorProject', convert_unicode=True)
+# engine = create_engine('mysql+pymysql://root:21bH1267@localhost/TutorProject', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+db = declarative_base()
+db.query = db_session.query_property()
+
+
+def init_db():
+    # import all modules here that might define models so that
+    # they will be registered properly on the metadata.  Otherwise
+    # you will have to import them first before calling init_db()
+    import models
+    db.metadata.create_all(bind=engine)
