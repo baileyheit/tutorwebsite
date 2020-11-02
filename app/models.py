e2d9c1089369d4cc8460c444dc50a98f07b41d29
Bailey Heit <bailey.heit@duke.edu>
2020-11-01 22:31:02 -0500

    fuck

--- app/models.py
+++ app/models.py
@@ -5,3 +5,6 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)    
