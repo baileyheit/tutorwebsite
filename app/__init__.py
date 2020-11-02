0f3c6866ace0071b3eefad066e0d108898f20832
Bailey Heit <bailey.heit@duke.edu>
2020-11-01 22:06:35 -0500

    bruh

--- app/__init__.py
+++ app/__init__.py
@@ -9,5 +9,5 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# from app import routes, models
-from app import WebService
+from app import routes, models
+# from app import WebService
