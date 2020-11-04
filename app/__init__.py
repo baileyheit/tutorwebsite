67d682eb01203fd039ba8a02e4feba693ae16786
VCM admin account <vcm@vcm-17138.vm.duke.edu>
2020-11-03 19:06:20 -0500

    should work now

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
@@ -9,4 +10,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# from app import routes, models
+login = LoginManager(app)
+login.login_view = 'login'
+
+from app import routes, models
