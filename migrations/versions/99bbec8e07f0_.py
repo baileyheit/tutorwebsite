c8bf4793e27a8bcf6692d048822e2dc2c87d1a12
Morgan Langenhagen <mel73@duke.edu>
2020-11-03 22:29:38 -0500

    packages

---
+++ migrations/versions/99bbec8e07f0_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 99bbec8e07f0
+Revises: b9fa419fc005, fe03e9077911
+Create Date: 2020-11-03 22:27:15.061935
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '99bbec8e07f0'
+down_revision = ('b9fa419fc005', 'fe03e9077911')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
