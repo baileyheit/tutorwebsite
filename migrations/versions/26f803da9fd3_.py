2815713d2fd4f6a425462cbcdf91dc88032271e9
Bailey Heit <bailey.heit@duke.edu>
2020-11-05 11:31:38 -0500

    add more to profile

---
+++ migrations/versions/26f803da9fd3_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 26f803da9fd3
+Revises: 
+Create Date: 2020-11-03 22:21:23.738131
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '26f803da9fd3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
