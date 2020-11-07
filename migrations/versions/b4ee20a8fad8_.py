12a6b5feac488f26d8bc3bd6f2b20a0ed6f347df
Bailey Heit <bailey.heit@duke.edu>
2020-11-07 13:06:17 -0500

    add password reset

---
+++ migrations/versions/b4ee20a8fad8_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: b4ee20a8fad8
+Revises: 
+Create Date: 2020-11-07 12:23:31.943040
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b4ee20a8fad8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
