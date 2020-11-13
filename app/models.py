a0462d13408bbb7b99215920bb32d885cacfd77e
Ava LeWinter <ava.lewinter@duke.edu>
2020-11-13 12:13:49 -0500

    final data

--- app/models.py
+++ app/models.py
@@ -30,7 +30,8 @@ class User(UserMixin, db.Model):
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
+    def check_password(self, password):from app import db
+from app.models import User, Session, Course, Cart, Rating
         return check_password_hash(self.password_hash, password)
 
     def avatar(self, size):
