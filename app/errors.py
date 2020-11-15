7259a01949f58a91bb66c9fa6145e01b982bae09
Bailey Heit <bailey.heit@duke.edu>
2020-11-15 17:21:30 -0500

    heroku deployment changes

--- app/errors.py
+++ app/errors.py
@@ -5,7 +5,7 @@ from app import app, db
 def not_found_error(error):
     return render_template('404.html'), 404
 
-@app.errorhandler(500)
+@app.errorhandler(505)
 def internal_error(error):
     db.session.rollback()
-    return render_template('500.html'), 500
+    return render_template('505.html'), 500
