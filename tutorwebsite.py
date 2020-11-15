4a62b118468734a8fccabfdbcd26b3b5f5c0619b
Bailey Heit <bailey.heit@duke.edu>
2020-11-15 10:40:23 -0500

    fix2

--- tutorwebsite.py
+++ tutorwebsite.py
@@ -4,4 +4,4 @@ from app.models import User
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'user': User, 'course': Course, 'session': Session, 'cart': Cart, 'rating': Rating:}
+    return {'db': db, 'user': User, 'course': Course, 'session': Session, 'cart': Cart, 'rating': Rating}
