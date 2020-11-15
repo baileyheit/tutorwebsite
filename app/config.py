7811c90ad990219bc99a01ff3a7627e20ebc6f34
Bailey Heit <bailey.heit@duke.edu>
2020-11-15 17:17:18 -0500

    heroku deployment changes

--- app/config.py
+++ app/config.py
@@ -1,20 +1,22 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db') or 'postgres://bykmhcasyanqyu:9fdac4102202b0972705aef15548b487b7c429442dfe5f0112fb1a096f90ac05@ec2-54-82-208-124.compute-1.amazonaws.com:5432/dqbjllmcnf1k3'
-    # HEROKU_POSTGRESQL_ONYX_URL = 'postgresql-cubic-74647'
-    # DATABASE_URL = 'postgresql-cubic-74647'
-    # postgresql-cubic-74647 as HEROKU_POSTGRESQL_ONYX_UR
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = True
-    DEBUG = True
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
-    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+    LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    POSTS_PER_PAGE = 25
