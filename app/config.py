e384741c6fbcab716427fafeffe9d96fa6b46f80
Bailey Heit <bailey.heit@duke.edu>
2020-11-15 16:42:49 -0500

    fix urls

--- app/config.py
+++ app/config.py
@@ -5,8 +5,8 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or os.environ.get('HEROKU_POSTGRESQL_ONYX_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db') 
-    HEROKU_POSTGRESQL_ONYX_URL = 'postgresql-cubic-74647'
-    DATABASE_URL = 'postgresql-cubic-74647'
+    # HEROKU_POSTGRESQL_ONYX_URL = 'postgresql-cubic-74647'
+    # DATABASE_URL = 'postgresql-cubic-74647'
     # postgresql-cubic-74647 as HEROKU_POSTGRESQL_ONYX_UR
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
